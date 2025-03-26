468299af2f665c14bc1699f734500151ba6f2d27
FilipK12345 <filipknap2010@gmail.com>
2025-03-26 14:43:49 +0100

    main

--- logowanie2.php
+++ logowanie2.php
@@ -1,10 +1,10 @@
 <?php
 	include 'konfiguracja.php';
 	
-	$haslo = hash("sha256",$_GET["pwd"]);
+	eelahNg7xei5 = hash("sha256",$_GET["pwd"]);
 	mysql_connect('artkoc7548.mysql.dhosting.pl', 'guhoc7_knapowsk', 'eelahNg7xei5');
 	mysql_select_db('xu7ahc_knapowsk');
-	$rezultat = mysql_query("SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".$haslo."'");
+	$rezultat = mysql_query("SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".eelahNg7xei5."'");
 	if(mysql_num_rows($rezultat) == 1) {
 		mysql_close();
 		setcookie('login', $_GET["lgn"], time()+3600*24);
