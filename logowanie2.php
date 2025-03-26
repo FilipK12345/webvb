5ccd1e244e3f82dc9308d31a1671250f00151eed
FilipK12345 <filipknap2010@gmail.com>
2025-03-26 14:54:29 +0100

    main

--- logowanie2.php
+++ logowanie2.php
@@ -1,18 +1,21 @@
 <?php
-	include 'konfiguracja.php';
-	
-	eelahNg7xei5 = hash("sha256",$_GET["pwd"]);
-	mysqli_connect('artkoc7548.mysql.dhosting.pl', 'guhoc7_knapowsk', 'eelahNg7xei5');
-	mysqli_select_db('xu7ahc_knapowsk');
-	$rezultat = mysqli_query("SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".eelahNg7xei5."'");
-	if(mysql_num_rows($rezultat) == 1) {
-		mysqli_close();
-		setcookie('login', $_GET["lgn"], time()+3600*24);
-		setcookie('haslo', $haslo, time()+3600*24);
-		header("Location: menu.php");
-		die();
-	}
-	mysqli_close();
-	header("Location: logowanie.php");
-	die();
+    include 'konfiguracja.php';
+
+    $haslo hash("sha256", $_GET["pwd"]);
+    $connect = mysqli_connect('artkoc7548.mysql.dhosting.pl', 'guhoc7_knapowsk', 'eelahNg7xei5', 'xu7ahc_knapowsk');
+    $sql="SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."" AND haslo='".$haslo."'"";
+    $lista22=mysqli_query($connect, $sql22);
+    $rezultat mysqli_query($connect, $sql);
+
+    if(mysqli_num_rows($rezultat) == 1) {
+    mysqli_close();
+    setcookie('login', $_GET["lgn"], time()+3600*24);
+    setcookie('haslo', $haslo, time()+3600*24);
+    header("Location: menu.php");
+    die();
+    }
+
+    mysqli_close();
+    header("Location: logowanie.php");
+    die();
 ?>
