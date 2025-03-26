db51fd45074af655c78badda59749a31b643e5f2
FilipK12345 <filipknap2010@gmail.com>
2025-03-26 14:35:14 +0100

    main

--- logowanie2.php
+++ logowanie2.php
@@ -2,8 +2,8 @@
 	include 'konfiguracja.php';
 	
 	$haslo = hash("sha256",$_GET["pwd"]);
-	mysql_connect($bazaAdres,$bazaLogin,$basaHaslo);
-	mysql_select_db($bazaNazwa);
+	mysql_connect('artkoc7548.mysql.dhosting.pl', 'guhoc7_knapowsk', 'eelahNg7xei5');
+	mysql_select_db('xu7ahc_knapowsk');
 	$rezultat = mysql_query("SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."' AND haslo='".$haslo."'");
 	if(mysql_num_rows($rezultat) == 1) {
 		mysql_close();
