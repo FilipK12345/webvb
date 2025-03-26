4ea8a37f8c601185ff9095e3e0812b8dfbd87772
FilipK12345 <filipknap2010@gmail.com>
2025-03-26 15:42:56 +0100

    main

--- logowanie2.php
+++ logowanie2.php
@@ -3,11 +3,11 @@
 
     $haslo hash("sha256", $_GET["pwd"]);
 
-    echo $haslo
+    echo $haslo;
 
-    $connect = mysqli_connect('artkoc7548.mysql.dhosting.pl', 'guhoc7_knapowsk', 'eelahNg7xei5', 'xu7ahc_knapowsk');
-    $sql="SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."" AND haslo='".$haslo."'"";
-    $lista22=mysqli_query($connect, $sql22);
+   // $connect = mysqli_connect('artkoc7548.mysql.dhosting.pl', 'guhoc7_knapowsk', 'eelahNg7xei5', 'xu7ahc_knapowsk');
+   // $sql="SELECT id FROM uzytkownicy WHERE login='".$_GET["lgn"]."" AND haslo='".$haslo."'"";
+   // $lista22=mysqli_query($connect, $sql22);
     //$rezultat mysqli_query($connect, $sql);
 
     //if(mysqli_num_rows($rezultat) == 1) {
@@ -17,7 +17,7 @@
     //header("Location: menu.php");
     //die();
     //}
-    mysqli_close($connect);
+    //mysqli_close($connect);
     //header("Location: logowanie.php");
     //die();
 ?>
